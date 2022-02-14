49a6c30059d63c8c1c5cfad54f9b39b256a5e639
Paras <parasthind299@gmail.com>
2022-02-14 17:11:41 +0530

    add item

--- src/renderer/components/AddItem.tsx
+++ src/renderer/components/AddItem.tsx
@@ -1,17 +1,37 @@
 /* eslint-disable prettier/prettier */
+/* eslint-disable jsx-a11y/no-autofocus */
+/* eslint-disable prettier/prettier */
 import { useContext } from 'react';
 import { AppContext } from 'renderer/context/Main.context';
 
 export default function AddItem() {
   const context = useContext(AppContext);
+
+  const handleAdd = () => {
+    context?.setItems((items) => {
+      items.push({
+        title: 'Hello',
+        screenshot: '',
+        id: context?.items.length + 1,
+      });
+      return items;
+    });
+
+    context?.setModal(false);
+  };
+
   return context?.modal ? (
     <>
       <div id="modal">
-        <input type="text" id="url" placeholder="Enter URL" />
-        <button type="button" id="add-item">
+        <input type="text" id="url" placeholder="Enter URL" autoFocus />
+        <button type="button" id="add-item" onClick={() => handleAdd()}>
           Add Item
         </button>
-        <button type="button" id="close-modal" onClick={() => context?.setModal(false)}>
+        <button
+          type="button"
+          id="close-modal"
+          onClick={() => context?.setModal(false)}
+        >
           Cancel
         </button>
       </div>
--- src/renderer/context/Main.context.tsx
+++ src/renderer/context/Main.context.tsx
@@ -1,11 +1,26 @@
 /* eslint-disable prettier/prettier */
-import { createContext, Dispatch, ReactFragment, useState } from 'react';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable prettier/prettier */
+import {
+  createContext,
+  Dispatch,
+  ReactFragment,
+  useState,
+  useEffect,
+  SetStateAction,
+} from 'react';
+
+export type ItemType = {
+  id: number;
+  screenshot: string;
+  title: string;
+};
 
 interface AppContextInterface {
   modal: boolean;
-  items: object[];
+  items: ItemType[];
   setModal: Dispatch<boolean>;
-  setItems: Dispatch<[]>;
+  setItems: Dispatch<SetStateAction<ItemType[]>>;
 }
 
 interface Props {
@@ -16,7 +31,14 @@ export const AppContext = createContext<AppContextInterface | null>(null);
 
 export const Provider = ({ children }: Props) => {
   const [modal, setModal] = useState(false);
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<ItemType[]>(
+    JSON.parse(localStorage.getItem('readit-items')!) || []
+  );
+
+  useEffect(() => {
+    if (items.length)
+      localStorage.setItem('readit-items', JSON.stringify(items));
+  }, [items, items.length]);
 
   return (
     <AppContext.Provider value={{ modal, setModal, items, setItems }}>
